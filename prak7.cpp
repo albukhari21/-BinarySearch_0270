fe7e64c62c821d08984780d1f690ab2b7c18ed48
albukhari21 <albukhariinsankamil@gmail.com>
2026-04-17 15:44:32 +0700

    Add input function with array size validation

--- prak7.cpp
+++ prak7.cpp
@@ -4,3 +4,29 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi." << endl;
+        }
+    }
+
+    cout << "=============================" << endl;
+    cout << "Masukkan elemen-elemen array:" << endl;
+    cout << "=============================" << endl;
+    
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> element[i];
