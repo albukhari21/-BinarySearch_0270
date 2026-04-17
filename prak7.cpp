039825a0ab3424366c460e569b10792e9cf14a9a
albukhari21 <albukhariinsankamil@gmail.com>
2026-04-17 15:46:52 +0700

    Implement element comparison and swapping logic

--- prak7.cpp
+++ prak7.cpp
@@ -37,3 +37,12 @@ void bubbleSortArray()
 {
     int pass = 1;
     do
+    {
+        for (int j = 0; j < nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j + 1];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
